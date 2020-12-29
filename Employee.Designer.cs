625e4b75a61d3678d7f6d09c54b49f7a9232e9d3
yauhescha <evgescha.maslov007@yandex.ru>
2020-12-29 15:21:03 +0300

    Добавлена работа с сотрудниками

--- Employee.Designer.cs
+++ Employee.Designer.cs
@@ -28,22 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.businesstripcounterDataSet = new BusinessTripCounter.businesstripcounterDataSet();
+            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeTableAdapter = new BusinessTripCounter.businesstripcounterDataSetTableAdapters.employeeTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fioDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.positionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.positionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.positionTableAdapter = new BusinessTripCounter.businesstripcounterDataSetTableAdapters.positionTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.businesstripcounterDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.positionBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.fioDataGridViewTextBoxColumn,
+            this.positionDataGridViewTextBoxColumn,
+            this.Column1});
+            this.dataGridView1.DataSource = this.employeeBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(257, 14);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(373, 144);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(239, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Изменить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_OnlyNumbers);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ФИО";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Должность";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.positionBindingSource;
+            this.comboBox1.DisplayMember = "name";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(82, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(169, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.ValueMember = "id";
+            // 
+            // businesstripcounterDataSet
+            // 
+            this.businesstripcounterDataSet.DataSetName = "businesstripcounterDataSet";
+            this.businesstripcounterDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeBindingSource
+            // 
+            this.employeeBindingSource.DataMember = "employee";
+            this.employeeBindingSource.DataSource = this.businesstripcounterDataSet;
+            // 
+            // employeeTableAdapter
+            // 
+            this.employeeTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Visible = false;
+            this.idDataGridViewTextBoxColumn.Width = 40;
+            // 
+            // fioDataGridViewTextBoxColumn
+            // 
+            this.fioDataGridViewTextBoxColumn.DataPropertyName = "fio";
+            this.fioDataGridViewTextBoxColumn.HeaderText = "ФИО";
+            this.fioDataGridViewTextBoxColumn.Name = "fioDataGridViewTextBoxColumn";
+            this.fioDataGridViewTextBoxColumn.ReadOnly = true;
+            this.fioDataGridViewTextBoxColumn.Width = 59;
+            // 
+            // positionDataGridViewTextBoxColumn
+            // 
+            this.positionDataGridViewTextBoxColumn.DataPropertyName = "position";
+            this.positionDataGridViewTextBoxColumn.HeaderText = "position";
+            this.positionDataGridViewTextBoxColumn.Name = "positionDataGridViewTextBoxColumn";
+            this.positionDataGridViewTextBoxColumn.ReadOnly = true;
+            this.positionDataGridViewTextBoxColumn.Visible = false;
+            this.positionDataGridViewTextBoxColumn.Width = 68;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Должность";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 90;
+            // 
+            // positionBindingSource
+            // 
+            this.positionBindingSource.DataMember = "position";
+            this.positionBindingSource.DataSource = this.businesstripcounterDataSet;
+            // 
+            // positionTableAdapter
+            // 
+            this.positionTableAdapter.ClearBeforeFill = true;
             // 
             // Employee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(642, 170);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
             this.Name = "Employee";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Сотрудники";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Employee_FormClosing);
             this.Load += new System.EventHandler(this.Employee_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.businesstripcounterDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.positionBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private businesstripcounterDataSet businesstripcounterDataSet;
+        private System.Windows.Forms.BindingSource employeeBindingSource;
+        private businesstripcounterDataSetTableAdapters.employeeTableAdapter employeeTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fioDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn positionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.BindingSource positionBindingSource;
+        private businesstripcounterDataSetTableAdapters.positionTableAdapter positionTableAdapter;
     }
 }
--- Employee.cs
+++ Employee.cs
@@ -19,12 +19,125 @@ namespace BusinessTripCounter
 
         private void Employee_Load(object sender, EventArgs e)
         {
-
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "businesstripcounterDataSet.position". При необходимости она может быть перемещена или удалена.
+            this.positionTableAdapter.Fill(this.businesstripcounterDataSet.position);
+            fillGrid();
         }
 
         private void Employee_FormClosing(object sender, FormClosingEventArgs e)
         {
             Main.main.Show();
+        }
+        private void fixName()
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                comboBox1.SelectedItem = comboBox1.Items[
+                                                           positionBindingSource.Find(
+                                                                       "id",
+                                                                       int.Parse(dataGridView1[2, i].Value.ToString())
+                                                                       )
+                                                           ];
+                dataGridView1[3, i].Value = comboBox1.Text;
+            }
+        }
+
+        private void textBox_OnlyNumbers(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (Char.IsDigit(number)) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void fillGrid()
+        {
+            this.employeeTableAdapter.Fill(this.businesstripcounterDataSet.employee);
+            fixName();
+        }
+        private bool isFill()
+        {
+            if (textBox1.Text.Length < 1 || comboBox1.Items.Count<1)
+            {
+                MessageBox.Show("Не все поля заполнены!");
+                return false;
+            }
+            return true;
+        }
+        private void clearFields()
+        {
+            textBox1.Text = "";
+        }
+        //add
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isFill())
+                try
+                {
+                    DataRowView row = (DataRowView)employeeBindingSource.AddNew();
+
+                    row[1] = textBox1.Text;
+                    row[2] = comboBox1.SelectedValue;
+
+                    employeeBindingSource.EndEdit();
+                    this.employeeTableAdapter.Update(businesstripcounterDataSet);
+                    clearFields();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            fillGrid();
+        }
+        //edit
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isFill())
+                try
+                {
+                    dataGridView1.CurrentRow.Cells[1].Value = textBox1.Text;
+                    dataGridView1.CurrentRow.Cells[2].Value = comboBox1.SelectedValue;
+                    dataGridView1.CurrentRow.Cells[3].Value = comboBox1.Text;
+                    employeeBindingSource.EndEdit();
+                    this.employeeTableAdapter.Update(((DataRowView)dataGridView1.CurrentRow.DataBoundItem).Row);
+
+                    clearFields();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            fillGrid();
+        }
+
+        //delete
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0 && dataGridView1.CurrentRow != null)
+            {
+                try
+                {
+                    businesstripcounterDataSet.AcceptChanges();
+                    employeeBindingSource.RemoveAt(dataGridView1.CurrentRow.Index);
+                    employeeBindingSource.EndEdit();
+                    employeeTableAdapter.Update(businesstripcounterDataSet.employee);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                fillGrid();
+            }
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0 && dataGridView1.CurrentRow != null)
+            {
+                textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                comboBox1.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            }
         }
     }
 }
